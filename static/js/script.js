cbedcc372b6ee5fd8720945fff6308ec3a2199ee
L II <lii@Ls-MacBook-Pro.lan>
2021-12-21 17:53:45 -0600

    updated image margin and padding

--- static/js/script.js
+++ static/js/script.js
@@ -19,5 +19,8 @@ function reset() {
 // Challenge 2: Cat Generator
 
 function generateCat() {
-
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src ='http://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+    div.appendChild(image)
 }
