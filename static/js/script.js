bb40dab35e1fbabe51ed9697abd091b0ca803e06
L II <lii@Ls-MacBook-Pro.lan>
2021-12-21 16:49:17 -0600

    updated button with funciton

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,13 @@
 // Challenge 1: Age in Days
 
-function age
+function ageInDays() {
+    var birthYear = prompt("What year were you born... Good friend?");
+    var ageDay = (2022 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageDay + ' days old');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    
+    // console.log(ageDay);
+}
