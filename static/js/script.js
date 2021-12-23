ca1334739bd5ca5017115505881ee2f907d822cc
L II <lii@Ls-MacBook-Pro.lan>
2021-12-23 01:15:38 -0600

    added buttonColorReset function

--- static/js/script.js
+++ static/js/script.js
@@ -134,3 +134,11 @@ function buttonsGreen() {
       }
     
 }
+
+function buttonColorReset() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+      }
+}
+
