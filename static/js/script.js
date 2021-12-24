f90dad3aba35bba520ecf300cd6cd744e771bb94
L II <lii@Ls-MacBook-Pro.lan>
2021-12-23 18:39:14 -0600

    updated path of images and sounds for blackjack challenge

--- static/js/script.js
+++ static/js/script.js
@@ -179,9 +179,9 @@ let blackjackGame = {
   
   document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
   
-  const hitSound = new Audio('/Users/lii/repos/javascript-app/static/sounds/swish.m4a');
-  const winSound = new Audio('/Users/lii/repos/javascript-app/static/sounds/cash.mp3');
-  const lossSound = new Audio('/Users/lii/repos/javascript-app/static/sounds/aww.mp3');
+  const hitSound = new Audio('/static/sounds/swish.m4a');
+  const winSound = new Audio('/static/sounds/cash.mp3');
+  const lossSound = new Audio('/static/sounds/aww.mp3');
   
   function blackjackHit() {
     if (blackjackGame['isStand'] === false) {
@@ -212,7 +212,7 @@ let blackjackGame = {
   function showCard(card, activePlayer) {
     if (activePlayer['score'] <= 21) {
       let cardImage = document.createElement('IMG');
-      cardImage.src = `/Users/lii/repos/javascript-app/static/images/${card}.png`
+      cardImage.src = `/static/images/${card}.png`
       document.querySelector(activePlayer['div']).appendChild(cardImage);
       hitSound.play();
     }
